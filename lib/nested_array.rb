fc89d65d755d9ad2001cdafe48ba9aa46e2dd987
Cesar Lara <cesar.lara@yale.edu>
2020-05-21 16:00:53 +0000

    Automatically backed up by Learn

--- lib/nested_array.rb
+++ lib/nested_array.rb
@@ -35,7 +35,7 @@ end
 
 def matrix_lookup(matrix, row, column)
 
-  matrix_lookup = (sorted_matrix, 0, 1)
+  matrix_lookup = sorted_matrix[0][1]
 
   # Given any matrix (array of arrays), a row index and a column index,
   # Return the matrix's content at that row and and column

f7e21d61b33db0028b348d4780e73baf9a227039
Cesar Lara <cesar.lara@yale.edu>
2020-05-21 15:33:39 +0000

    Automatically backed up by Learn

--- lib/nested_array.rb
+++ lib/nested_array.rb
@@ -35,7 +35,7 @@ end
 
 def matrix_lookup(matrix, row, column)
 
-  matrix_lookup[0][1]
+  matrix_lookup = CONVENTIONAL_PRODUCE[0]
   # Given any matrix (array of arrays), a row index and a column index,
   # Return the matrix's content at that row and and column
 end
